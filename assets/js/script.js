a1bd9ddd12662a4de2a715ef9e02861d051e5d6b
MariaJose <mjroolma@gmail.com>
2018-10-09 03:26:14 -0300

    formulario js

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,28 +4,7 @@ $(window).ready(function(){
 			alert("Bienvenido")
 		});
 		
-
-//		$(window).on('load', function(){
-//			alert("Bienvenido")
-//		});
-
-
-// --intento de corazon--
-
-//$('.corazon__gris').click( function(e){
-//		e.preventDefault();
-//		$(this).toggleClass('.corazon-rojo');
-
-//	});
-
-//	$('.corazon__gris').on('click', function(e){
-
-//		e.preventDefault();
-//		e.stopPropagation();
-
-//	});
-
-
+//me gusta//
 		
  $('body').on('click', '.corazon__gris', function () {
         $(this).children('i').css('color', 'red');
@@ -33,5 +12,22 @@ $(window).ready(function(){
         numero = numero + 1;
         $(this).children('span').text(numero);
     });
+
+ //formulario//
+
+ var Form = document.getElementById('form');
+
+form .addEventListener('submit', function(e){
+	e.preventDefault();
+	console.log('hiciste click')
+
+	var datos = new FormData(form);
+
+	console.log(datos)
+	console.log(datos.get('usuario'))
+	console.log(datos.get('email'))
+	console.log(datos.get('medidas'))
+	console.log(datos.get('text-area'))
+});
 
 }); 
